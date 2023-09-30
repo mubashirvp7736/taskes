6f9bbd480b48692911ad17f077261202b98b81d7
mubashirvp7736 <vpmubashir384@gmail.com>
2023-09-30 05:18:09 -1200

    first commit

---
+++ assignment10.dart
@@ -0,0 +1,16 @@
+// import 'dart:io';
+
+// void main() {
+//   print("Enter number");
+//   int number = int.parse(stdin.readLineSync()!);
+//   print("Enter power");
+//   int power = int.parse(stdin.readLineSync()!);
+//   int result = 1;
+//   for (int i = 0; i < power; i++) {
+//     result *= number;
+//   }
+//   print("$number ^ $power = $result");
+// }
+// for(i=0 ;i<power;i++){
+//   result*=Number
+// }
